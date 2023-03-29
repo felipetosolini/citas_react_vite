deebe74ae425aed63e89d3fb95c8a785480fa639
felipetosolini <felipetosolini@gmail.comm>
2023-03-29 17:45:55 -0300

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import ListadoPacientes from "./ListadoPacientes"
 function Header() {
     return(
         <h1 className="font-black text-5xl text-center md:w-2/4 mx-auto">
-            Seguimiento Pacientes {""}
+            Seguimiento Pacientes {""} Con Github{""}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
